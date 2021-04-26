6796387f8843cf77f341378844f068626c7f0cb6
Andrei Sosea <sosea.andrei@yahoo.com>
2021-04-26 11:10:08 +0300

    labtasks for http lab

--- labtasks/GoogleSearcher/app/src/main/java/ro/pub/cs/systems/eim/lab07/googlesearcher/network/GoogleSearcherAsyncTask.java
+++ labtasks/GoogleSearcher/app/src/main/java/ro/pub/cs/systems/eim/lab07/googlesearcher/network/GoogleSearcherAsyncTask.java
@@ -1,7 +1,18 @@
 package ro.pub.cs.systems.eim.lab07.googlesearcher.network;
 
 import android.os.AsyncTask;
+import android.util.Log;
 import android.webkit.WebView;
+
+import java.io.IOException;
+
+import cz.msebera.android.httpclient.client.ClientProtocolException;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.ResponseHandler;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.impl.client.BasicResponseHandler;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import ro.pub.cs.systems.eim.lab07.googlesearcher.general.Constants;
 
 public class GoogleSearcherAsyncTask extends AsyncTask<String, Void, String> {
 
@@ -20,6 +31,22 @@ public class GoogleSearcherAsyncTask extends AsyncTask<String, Void, String> {
         // create an instance of a ResponseHandler object
         // execute the request, thus generating the result
 
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(Constants.GOOGLE_INTERNET_ADDRESS + params[0]);
+        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+        try {
+            return httpClient.execute(httpGet, responseHandler);
+        } catch (ClientProtocolException clientProtocolException) {
+            Log.e(Constants.TAG, clientProtocolException.getMessage());
+            if (Constants.DEBUG) {
+                clientProtocolException.printStackTrace();
+            }
+        } catch (IOException ioException) {
+            Log.e(Constants.TAG, ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        }
         return null;
     }
 
@@ -34,5 +61,11 @@ public class GoogleSearcherAsyncTask extends AsyncTask<String, Void, String> {
         // - encoding is UTF-8
         // - history is null
 
+        googleResultsWebView.loadDataWithBaseURL(
+                Constants.GOOGLE_INTERNET_ADDRESS,
+                content,
+                Constants.MIME_TYPE,
+                Constants.CHARACTER_ENCODING,
+                null);
     }
 }
